b18a45580739032168750fce3f78a145aa1b8e2a
SAPR123 <SAPR@PC-NXT>
2025-09-24 15:14:16 +0700

    На паре поделала визуал, все сьехало уже не успею подправить. Сохраню мб все-таки пригодится

--- Game/MainWindow.xaml.cs
+++ Game/MainWindow.xaml.cs
@@ -20,5 +20,10 @@ namespace Game
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
